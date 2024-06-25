fb43fc9dfd7225ccf04dfa5b1f19ddcd84cd0774
Fanny Lucas <fanny.lucas@csedge.org>
2024-06-25 14:53:13 -0700

    created madlib!!

--- madlibs.py
+++ madlibs.py
@@ -1,29 +1,78 @@
 #Madlibs by Fanny
 
-import random
+#import random
  
-adjectiveS = ["silly", "giant", "small", "annoying", "ashamed", "awful"]
-emotionS = ["sad", "happy", "furious", "anxious", "disgusted", "suprised"]
-foodItemS = ["popcorn", "icecream", "apple", "hotdogs", "pizza"]
-rideNameS = ["bumped cars", "Haunted House", "Ferris Wheel", "Roller coaster"]
-exclamationS = ["WOW!", "OUCH!!", "WHEEEE!!!","WOAHHHH"]
-verbINGS = ["eating", "walking", "running", "reading", "laughing"]
+#adjectiveS = ["silly", "giant", "small", "annoying", "ashamed", "awful"]
+#emotionS = ["sad", "happy", "furious", "anxious", "disgusted", "suprised"]
+#foodItemS = ["popcorn", "icecream", "apple", "hotdogs", "pizza"]
+#rideNameS = ["bumped cars", "Haunted House", "Ferris Wheel", "Roller coaster"]
+#exclamationS = ["WOW!", "OUCH!!", "WHEEEE!!!","WOAHHHH"]
+#verbINGS = ["eating", "walking", "running", "reading", "laughing"]
+
+#adjective = input("Enter an adjective or press Enter for a random word")
+#if not adjective:
+    #print(random.choice(adjectiveS))
+#emotion = input("Enter an adjective or press Enter for a random word")
+#if not emotion: 
+    #print(random.choice(emotionS))
+#emotion2 = input("Enter an adjective or press Enter for a random word")
+#if not emotion2:
+    #print(random.choice(emotionS))
+#adjective2 = input("Enter an adjective or press Enter for a random word")
+#if not adjective2: 
+    #print(random.choice(adjectiveS))
+#adjective3 = input("Enter an adjective or press Enter for a random word")
+#if not adjective3: 
+    #print(random.choice(adjectiveS))
+#foodItem =  input("Enter an food item or press Enter for a random word")
+#if not foodItem: 
+    #print(random.choice(foodItemS))
+#adjective4 = input("Enter an adjective or press Enter for a random word")
+#if not adjective4: 
+    #print(random.choice(adjectiveS))
+#exclamation = input("Enter an exclamation or press Enter for a random word")
+#if not exclamation: 
+    #print(random.choice(exclamationS))
+#rideName = input("Enter a ride name or press Enter for a random word")
+#if not rideName: 
+    #print(random.choice(rideNameS))
+#rideName2 = input("Enter a ride name or press Enter for a random word")
+#if not rideName2: 
+    #print(random.choice(rideNameS))
+#verbING = input("Enter a verb ending with ING or press Enter for a random word")
+#if not verbING: 
+    #print(random.choice(verbINGS))
+#foodItem2 = input("Enter a food item or press Enter for a random word")
+#if not foodItem2: 
+    #print(random.choice(foodItemS))
 
 
+#adjective = random.choice(adjectiveS)
+#emotion = random.choice(emotionS)
+#emotion2 = random.choice(emotionS)
+#adjective2 = random.choice(adjectiveS)
+#adjective3 = random.choice(adjectiveS)
+#foodItem = random.choice(foodItemS)
+#adjective4 = random.choice(adjectiveS)
+#exclamation = random.choice(exclamationS)
+#rideName = random.choice(rideNameS)
+#rideName2 = random.choice(rideNameS)
+#verbING  = random.choice(verbINGS)
+#foodItem2 = random.choice(foodItemS)
 
-adjective = random.choice(adjectiveS)
-emotion = random.choice(emotionS)
-emotion2 = random.choice(emotionS)
-adjective2 = random.choice(adjectiveS)
-adjective3 = random.choice(adjectiveS)
-foodItem = random.choice(foodItemS)
-adjective4 = random.choice(adjectiveS)
-exclamation = random.choice(exclamationS)
-rideName = random.choice(rideNameS)
-rideName2 = random.choice(rideNameS)
-verbING  = random.choice(verbINGS)
-foodItem2 = random.choice(foodItemS)
 
+adjective = input("Enter an adjective ")
+emotion = input("Enter an adjective ")
+emotion2 = input("Enter an adjective ")
+adjective2 = input("Enter an adjective ")
+adjective3 = input("Enter an adjective ")
+foodItem =  input("Enter an food item ")
+adjective4 = input("Enter an adjective ")
+exclamation = input("Enter an exclamation ")
+rideName = input("Enter a ride name ")
+rideName2 = input("Enter a ride name ")
+verbING = input("Enter a verb ending with ING")
+foodItem2 = input("Enter a food item ")
 
 
 print(f"Today was a(n) {adjective} day for me. I woke up feeling {emotion} and ready for an adventure.\n My friends and I planned to go to the {adjective2} amusement park in town. As we approached the entrance,\n we could hear the {adjective3} music playing and smell the delicous scent of {foodItem} in the air.\n Our first stop was the {rideName}.\n I felt {emotion2} as we climbed higher and higher, then screamed {exclamation} as we zoomed down the steep drop.\n Next we headed to the {rideName2}, where i couldnt stop {verbING} we spun around.\n After all the excitement, we decided to refuel with {foodItem2}")
