e0e2168db40bd79d9a688028283cd41a44a7e80e
Fanny Lucas <fanny.lucas@csedge.org>
2024-06-25 14:22:36 -0700

    first commit :D

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,50 @@
-#Madlibs by Fanny 
+#Madlibs by Fanny
 
-print("Here is my first commit :D")
+import random
+ 
+adjectiveS = ["silly", "giant", "small", "annoying", "ashamed", "awful"]
+emotionS = ["sad", "happy", "furious", "anxious", "disgusted", "suprised"]
+foodItemS = ["popcorn", "icecream", "apple", "hotdogs", "pizza"]
+rideNameS = ["bumped cars", "Haunted House", "Ferris Wheel", "Roller coaster"]
+exclamationS = ["WOW!", "OUCH!!", "WHEEEE!!!","WOAHHHH"]
+verbINGS = ["eating", "walking", "running", "reading", "laughing"]
+
+
+
+adjective = random.choice(adjectiveS)
+emotion = random.choice(emotionS)
+emotion2 = random.choice(emotionS)
+adjective2 = random.choice(adjectiveS)
+adjective3 = random.choice(adjectiveS)
+foodItem = random.choice(foodItemS)
+adjective4 = random.choice(adjectiveS)
+exclamation = random.choice(exclamationS)
+rideName = random.choice(rideNameS)
+rideName2 = random.choice(rideNameS)
+verbING  = random.choice(verbINGS)
+foodItem2 = random.choice(foodItemS)
+
+
+
+print(f"Today was a(n) {adjective} day for me. I woke up feeling {emotion} and ready for an adventure.\n My friends and I planned to go to the {adjective2} amusement park in town. As we approached the entrance,\n we could hear the {adjective3} music playing and smell the delicous scent of {foodItem} in the air.\n Our first stop was the {rideName}.\n I felt {emotion2} as we climbed higher and higher, then screamed {exclamation} as we zoomed down the steep drop.\n Next we headed to the {rideName2}, where i couldnt stop {verbING} we spun around.\n After all the excitement, we decided to refuel with {foodItem2}")
+
+
+#Today was a(n) __________ day for me. 
+# I woke up feeling __________ and ready for an adventure.
+#  My friends and I planned to go to the __________ amusement park in town.
+#  As we approached the entrance, 
+# we could hear the __________ music playing and smell
+#  the delicious scent of __________ in the air.
+
+#Our first stop was the __________ coaster. 
+# I felt __________ as we climbed higher and higher, 
+# then screamed __________ as we zoomed down the steep drop. 
+# Next, we headed to the __________ ride,
+#  where I couldn't stop __________ as we spun around.
+
+#After all the excitement, we decided to refuel with __________ and __________. As the sun set, we reflected on our day filled with __________ memories and promised to come back soon.
+
+#It was truly a(n) __________ day at the amusement park!
+
+
+
